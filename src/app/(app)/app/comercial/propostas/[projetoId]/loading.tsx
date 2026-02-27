ba6c78f1f2a999536375db2ca7310fa5bb4afd69
Victor Trevilato <victortrevilato@outlook.com>
2026-02-27 19:16:37 -0300

    update: Melhorias na Aprovação da Proposta V2

--- src/app/(app)/app/comercial/propostas/[projetoId]/loading.tsx
+++ src/app/(app)/app/comercial/propostas/[projetoId]/loading.tsx
@@ -33,12 +33,11 @@ export default function LoadingProposalsList() {
         <Table>
             <TableHeader>
               <TableRow className="bg-muted/40">
-                <TableHead>Bloco / Unidade</TableHead>
+                <TableHead>Unidade</TableHead>
                 <TableHead>Status</TableHead>
-                <TableHead>Comprador (Lead)</TableHead>
-                <TableHead>Cód. Tabela</TableHead>
-                <TableHead>Data Proposta</TableHead>
-                <TableHead>Validade</TableHead>
+                <TableHead>Comprador</TableHead>
+                <TableHead>Data da Proposta</TableHead>
+                <TableHead>Data de Validade</TableHead>
                 <TableHead className="text-right">Valor</TableHead>
                 <TableHead className="text-right">Ações</TableHead>
               </TableRow>
@@ -55,7 +54,6 @@ export default function LoadingProposalsList() {
                   <TableCell><Skeleton className="h-6 w-24 rounded-full" /></TableCell>
                   <TableCell><Skeleton className="h-5 w-48" /></TableCell>
                   <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                  <TableCell><Skeleton className="h-5 w-20" /></TableCell>
                   <TableCell><Skeleton className="h-5 w-20" /></TableCell>
                   <TableCell><div className="flex justify-end"><Skeleton className="h-5 w-28" /></div></TableCell>
                   <TableCell className="text-right">
